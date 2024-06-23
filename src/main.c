5a5255bf33e3feed8f5ab6dfbad7a03353660560
Pedro Ivo <98492435+vegedra@users.noreply.github.com>
2024-06-22 21:15:52 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -19,13 +19,13 @@
 
 // Estrutura de uma sala/local - room
 typedef struct {
-    char name[NAME_SIZE];
-    char description[DESC_SIZE];
-    int north;
-    int south;
-    int east;
-    int west;
-	int eventIndex;  // Evento que a sala terá (-1 se não houver)
+	    char name[NAME_SIZE];
+	    char description[DESC_SIZE];
+	    int north;
+	    int south;
+	    int east;
+	    int west;
+	    int eventIndex;  // Evento que a sala terá (-1 se não houver)
 } Room;
 
 typedef struct {
@@ -35,11 +35,11 @@ typedef struct {
 
 // Variavéis
 Room rooms[NUM_ROOMS];          // Array das Rooms
-Event events[NUM_EVENTS];	    // Array dos eventos
+Event events[NUM_EVENTS];       // Array dos eventos
 int currentRoomIndex = 0;       // Index da room atual
 char player_move[10];           // Input do movimento do jogador
 char name[9];                   // Nome do jogador
-int bonus;			            // Bonus que é calculado no SCORE
+int bonus;			// Bonus que é calculado no SCORE
 int steps = 0;                  // Contador de passos do jogador
 
 // Protótipos de função
@@ -59,64 +59,62 @@ int main(void) {
 
     // Inicializa as salas e eventos
     initializeRooms();
-	initializeEvents();
+    initializeEvents();
 	
-	int input;
+    int input;
     do {
-		curs_set(0);  // Esconde o cursor
-		resize_term(40, 80);  // Muda o tamanho da janela, tem que estar acompanhado de um refresh()
+	curs_set(0);  // Esconde o cursor
+	resize_term(40, 80);  // Muda o tamanho da janela, tem que estar acompanhado de um refresh()
         clear();
         mvprintw(0, 0, "********************************************************************************");
         mvprintw(1, 25, "EXIT THE DUNGEON IN 60 SECONDS");
         mvprintw(2, 31, "by Pedro Ivo, 2024");
         mvprintw(20, 36, "1) Play");
         mvprintw(21, 36, "2) Exit");
-		mvprintw(38, 75, "v1.0");
+	mvprintw(38, 75, "v1.0");
         mvprintw(39, 0, "********************************************************************************\n");
-		refresh();  // Atualiza a tela
+	refresh();  // Atualiza a tela
 
         // Recebe o input do jogador
         input = getch();
 
         if (input == '1') {
-			curs_set(1);
-
+	    curs_set(1);
             generateCharacter();
-			
-            time_t start_time = time(NULL);  // Get the current time
-            int remaining_time = 60;  // Timer set to 60 seconds
 
-			//timeout(1000);
+	    // Código do timer
+            time_t start_time = time(NULL); 
+            int remaining_time = 60;  
 			
             do {
-				// Calcula o tempo que passou
-				int elapsed_time = (int)difftime(time(NULL), start_time);
+		// Calcula o tempo que passou
+		int elapsed_time = (int)difftime(time(NULL), start_time);
                 remaining_time = 60 - elapsed_time;
 
-                handleMovement(remaining_time);  // Pass remaining time to handleMovement
+                handleMovement(remaining_time);  // Função que lida com a movimentação recebe remaining time
                 
                 if (remaining_time <= 0 && currentRoomIndex != 9) {
                     mvprintw(20, 0, "THE MONSTER GOT YOU! GAME OVER");
                     refresh();
                     napms(2000);
                     break;
-				}
+		}
 				
                 // Atualiza a tela
                 clear();
                 displayPlayerStats(remaining_time);
                 refresh();
 				
-				// Se o jogador chegou ao final
+		// Se o jogador chegou ao final
                 if (currentRoomIndex == 9) {
                     calculateScore(remaining_time);
                     refresh();
                     getch();
-					break;
+		    break;
                 }
-                
             } while (strcasecmp(player_move, "exit") != 0 && remaining_time > 0);
-			// Reset game state if player chooses to play again
+		
+	    // Reseta as varíaveis para uma nova jogatina
             currentRoomIndex = 0;
             bonus = 0;
             steps = 0;
@@ -132,104 +130,105 @@ int main(void) {
 
 // Inicia os eventos
 void initializeEvents() {
-	strcpy(events[0].description, "You found the secret!");
-	events[0].effect = +10;
+    // Eventos do jogo, no momento usados somente para os segredos/bonus
+    strcpy(events[0].description, "You found the secret!");
+    events[0].effect = +10;
 }
 
-// Inicia as rooms com seus nomes, descrições e conexões predefinidas
+// Inicia as rooms com seus nomes, descrições e conexões predefinidas, algum dia faço um sistema melhor
 void initializeRooms() {
     //strcpy(destino, fonte);  syntaxe
 
-	// Level 1
-	// A0
+    // Level 1
+    // A0
     strcpy(rooms[0].name, "is at the start");
     strcpy(rooms[0].description, "I can hear the monster...");
     rooms[0].north = -1; // Não
     rooms[0].south = -1; // Não
     rooms[0].east = 1;  // East leads to room 1 
     rooms[0].west = -1; // Não
-	rooms[0].eventIndex = -1;  // Sem evento
+    rooms[0].eventIndex = -1;  // Sem evento
 
-	// B1
+    // B1
     strcpy(rooms[1].name, "is in a corridor.");
     strcpy(rooms[1].description, "I'm hungry.");
     rooms[1].north = 2; 
     rooms[1].south = -1;  
     rooms[1].east = -1;  
     rooms[1].west = 0;  
-	rooms[1].eventIndex = -1;
+    rooms[1].eventIndex = -1;
 
-	// C2
+    // C2
     strcpy(rooms[2].name, "is in a corridor (again?)");
     strcpy(rooms[2].description, "Oh God.");
     rooms[2].north = 3; 
     rooms[2].south = 1; 
     rooms[2].east = -1;  
     rooms[2].west = 4;  
-	rooms[2].eventIndex = 2; 
+    rooms[2].eventIndex = 2; 
 
-	// D3
+    // D3
     strcpy(rooms[3].name, "is in a dead-end.");
     strcpy(rooms[3].description, "Opsie...");
     rooms[3].north = -1;
     rooms[3].south = 2;
     rooms[3].east = -1;  
     rooms[3].west = -1;   
-	rooms[3].eventIndex = -1;
+    rooms[3].eventIndex = -1;
 	
-	// E4
+    // E4
     strcpy(rooms[4].name, "is in a world of possibilities.");
     strcpy(rooms[4].description, "Which one...");
     rooms[4].north = 5;
     rooms[4].south = -1;
     rooms[4].east = 2;  
     rooms[4].west = 7;   
-	rooms[4].eventIndex = -1;
+    rooms[4].eventIndex = -1;
 	
-	// F5
+    // F5
     strcpy(rooms[5].name, "is so close!");
     strcpy(rooms[5].description, "...");
     rooms[5].north = 9;
     rooms[5].south = 4;
     rooms[5].east = -1;  
     rooms[5].west = 6;   
-	rooms[5].eventIndex = -1;
+    rooms[5].eventIndex = -1;
 	
-	// G6
+    // G6
     strcpy(rooms[6].name, "is in -- what's that?");
     strcpy(rooms[6].description, "Still a dead-end...");
     rooms[6].north = -1;
     rooms[6].south = -1;
     rooms[6].east = 5;  
     rooms[6].west = -1;   
-	rooms[6].eventIndex = 0;
+    rooms[6].eventIndex = 0;
 	
-	// H7
+    // H7
     strcpy(rooms[7].name, "is in -- where am I?");
     strcpy(rooms[7].description, "...?");
     rooms[7].north = -1;
     rooms[7].south = 8;
     rooms[7].east = 4;  
     rooms[7].west = -1;   
-	rooms[7].eventIndex = -1;
+    rooms[7].eventIndex = -1;
 	
-	// I8
+    // I8
     strcpy(rooms[8].name, "is in a dead-end.");
     strcpy(rooms[8].description, "Opsie...");
     rooms[8].north = 7;
     rooms[8].south = -1;
     rooms[8].east = -1;  
     rooms[8].west = -1;   
-	rooms[8].eventIndex = -1;
+    rooms[8].eventIndex = -1;
 	
-	// FINAL
+    // FINAL
     strcpy(rooms[9].name, "got out of the dungeon!");
     strcpy(rooms[9].description, "Congratulations!");
     rooms[9].north = -1;
     rooms[9].south = -1;
     rooms[9].east = -1;  
     rooms[9].west = -1;   
-	rooms[9].eventIndex = -1;
+    rooms[9].eventIndex = -1;
 }
 
 // Exibe a localização e a descrição atuais do jogador
@@ -237,8 +236,8 @@ void displayPlayerStats(int remaining_time) {
     mvprintw(0, 0, "%s %s | TIME LEFT: %d seconds.\n", name, rooms[currentRoomIndex].name, remaining_time);
     mvprintw(1, 0, "%s\n", rooms[currentRoomIndex].description);
 	
-	// debug
-	//mvprintw(10, 0, "player move: %s\n", player_move); 
+    // debug
+    //mvprintw(10, 0, "player move: %s\n", player_move); 
     
     // Verifica se há um evento associado à sala atual
     if (rooms[currentRoomIndex].eventIndex >= 0) {
@@ -309,7 +308,7 @@ void handleMovement(int remaining_time) {
 void calculateScore(int remaining_time) {
     int score = remaining_time * 10 + bonus - steps * 5;
 	
-	mvprintw(4, 0, "YOUR SCORE: \n");
+    mvprintw(4, 0, "YOUR SCORE: \n");
     mvprintw(6, 0, "SCORE: %d\n", score);
     mvprintw(7, 0, "Remaining Time Bonus: %d seconds * 10 = %d\n", remaining_time, remaining_time * 10);
     mvprintw(8, 0, "Events Bonus: +%d\n", bonus);
@@ -320,15 +319,15 @@ void calculateScore(int remaining_time) {
 void generateCharacter() {
     clear();
     mvprintw(0, 0, "What's your name?");
-	mvprintw(1, 0, "> ");
+    mvprintw(1, 0, "> ");
     refresh();
     scanw("%9s", name); 
 
     mvprintw(3, 0, "After some exploration in the dungeon, %s is coming back to the surface.\n"
-					"For their bad luck, a powerful monster wakes! %s now have to run for\n"
-					"their life! Will they be able to find their way out of this Labyrinth? \n", name, name);
-	refresh();
-	mvprintw(8, 0, "Press ENTER to start!");
+		   "For their bad luck, a powerful monster wakes! %s now have to run for\n"
+		   "their life! Will they be able to find their way out of this Labyrinth? \n", name, name);
+    refresh();
+    mvprintw(8, 0, "Press ENTER to start!");
     refresh();
     getch();
 }
